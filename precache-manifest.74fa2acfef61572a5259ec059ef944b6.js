cd9b420ec205150b143a68bc9ca942e1de8ffca1
Paliy Stepan <palstepweb@gmail.com>
2020-03-19 15:36:38 +0300

    test

---
+++ precache-manifest.74fa2acfef61572a5259ec059ef944b6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1aa07642fad72b2cf0998c7b4b40529",
+    "url": "/PaliyStepan/Samurai/index.html"
+  },
+  {
+    "revision": "f03b1e6af81bbf7fe364",
+    "url": "/PaliyStepan/Samurai/static/css/3.2c509ab8.chunk.css"
+  },
+  {
+    "revision": "e2941f5eacce150b0e2b",
+    "url": "/PaliyStepan/Samurai/static/css/4.f5bfdac4.chunk.css"
+  },
+  {
+    "revision": "c18d66f14818777ee90c",
+    "url": "/PaliyStepan/Samurai/static/css/5.b690f9e9.chunk.css"
+  },
+  {
+    "revision": "6ee75087fdf0b1395cde",
+    "url": "/PaliyStepan/Samurai/static/css/main.64f10ca8.chunk.css"
+  },
+  {
+    "revision": "d5e357f5ac0501e387a9",
+    "url": "/PaliyStepan/Samurai/static/js/2.731c4150.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/PaliyStepan/Samurai/static/js/2.731c4150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03b1e6af81bbf7fe364",
+    "url": "/PaliyStepan/Samurai/static/js/3.fab36d20.chunk.js"
+  },
+  {
+    "revision": "e2941f5eacce150b0e2b",
+    "url": "/PaliyStepan/Samurai/static/js/4.42c40626.chunk.js"
+  },
+  {
+    "revision": "c18d66f14818777ee90c",
+    "url": "/PaliyStepan/Samurai/static/js/5.52126e70.chunk.js"
+  },
+  {
+    "revision": "6ee75087fdf0b1395cde",
+    "url": "/PaliyStepan/Samurai/static/js/main.1c099b27.chunk.js"
+  },
+  {
+    "revision": "659083138fb6247cb8e7",
+    "url": "/PaliyStepan/Samurai/static/js/runtime-main.e7e191fc.js"
+  },
+  {
+    "revision": "29d7d247f9ff279af161350a26d61795",
+    "url": "/PaliyStepan/Samurai/static/media/loading.29d7d247.gif"
+  },
+  {
+    "revision": "1c9f98d164352992a7ab86f8ab682a4b",
+    "url": "/PaliyStepan/Samurai/static/media/user-img.1c9f98d1.png"
+  }
+]);
